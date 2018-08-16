9a514a933e9245800fe828049d53f0923f2c3a6b
“VitaliyNickishin” <nickishin34@gmail.com>
2018-08-17 00:57:58 +0300

    finished project

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,4 @@
 $(document).ready(function(){
-/*
- $('#nav li').hover(function(){
- 	$(this).children('ul').stop(false, true).fadeIn(300);
- },function(){
- 	$(this).children('ul').stop(false, true).fadeOut(300);
- });
- */
-
- /*
-$('#nav').click(function(){
- 	$('.sub').slideToggle()
- });
-*/
 
 /*открытие формы поиска*/
 
@@ -19,24 +6,11 @@ $('.search-button').click(function(){
   $('.sb').toggleClass('sb_opened');
 })
 
-/*закрытие формы поиска
-
-$(document).click(function(event) {
-    if ($(event.target).closest(".search-button").length ) return;
-    $('.sb').removeClass('sb_opened');
-    event.stopPropagation();
-});
-*/
 
 /*разворачивающаяся стрелка при открытии под меню*/
 $('#services').click(function(){
   $('.icon-angle-down').toggleClass('icon_close');
 })
-
-
-
-
-
 
 
 }); 
